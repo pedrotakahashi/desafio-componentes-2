8efa7d57601a7c5e36abb2fb89620325a129c1b0
pedrotakahashi <pedroh.takahashi@gmail.com>
2022-05-11 10:17:17 -0300

    creted shared state

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,38 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from "react";
+import { IContentProps } from "../Interfaces/IContentProps";
+import { IGenreResponseProps } from "../Interfaces/IGenreResponseProps";
+import { IMovieProps } from "../Interfaces/IMovieProps";
+import { ISideBarProps } from "../Interfaces/ISideBarProps";
+import { api } from "../services/api";
+import { Button } from "./Button";
+
+
+export function SideBar({selectedGenreId, handleClickButton}: ISideBarProps) {
+  const [genres, setGenres] = useState<IGenreResponseProps[]>([]);
+  
+  
+  useEffect(() => {
+    api.get<IGenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return(
+    <>
+      <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>     
+  </>
+  )
 }
